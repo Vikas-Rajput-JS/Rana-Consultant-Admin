d4bbdf9e31544785048e5d4a4cb18081b6ef02fa
davidthakur <vr384695@gmail.com>
2024-02-24 20:25:53 +0530

    changes

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 7041;
+
+// Serve static files from the build folder
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Define a route to serve the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
